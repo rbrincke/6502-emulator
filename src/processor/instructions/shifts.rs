3d679ebc68cd1ef062464e1cf50530f28e45fc5a
rbrincke <r.h.w.tenbrincke@gmail.com>
2020-06-13 09:53:43 +0200

    Wire up accumulator shifts + tests

--- src/processor/instructions/shifts.rs
+++ src/processor/instructions/shifts.rs
@@ -4,10 +4,23 @@ use crate::processor::registers::Flag;
 use crate::processor::Core;
 
 impl<C : Cartridge> Core<C> {
-    fn asl_core(&mut self, value: u8) -> u8 {
+    fn shift_accumulator<F : Fn(&mut Self, u8) -> u8>(&mut self, shift: F) {
+        let value = self.registers.accumulator;
+        let result = shift(self, value);
+        self.registers.accumulator = result;
+    }
+
+    fn shift_address<F : Fn(&mut Self, u8) -> u8>(&mut self, address_mode: AddressMode, shift: F) {
+        let address = self.address(address_mode);
+        let value = self.read_raw(address);
+        let result = shift(self, value);
+        self.write_raw(address, result);
+    }
+
+    fn asl(&mut self, value: u8) -> u8 {
         let result_intermediate = (value as u16) << 1;
         self.check_value_set_carry(result_intermediate);
-        let result = (value & 0xFF) as u8;
+        let result = (result_intermediate & 0xFF) as u8;
 
         self.check_value_set_zero(result);
         self.check_value_set_negative(result);
@@ -15,61 +28,76 @@ impl<C : Cartridge> Core<C> {
         result
     }
 
+    /// Arithmetic shift left on accumulator.
     pub(crate) fn asl_acc(&mut self) {
-        let result = self.asl_core(self.registers.accumulator);
-        self.registers.accumulator = result;
+        self.shift_accumulator(|r, v| r.asl(v));
     }
 
     /// Arithmetic shift left.
-    pub(crate) fn asl(&mut self, address_mode: AddressMode) {
-        let address = self.address(address_mode);
-        let value = self.read_raw(address);
-        let result = self.asl_core(value);
-        self.write_raw(address, result);
+    pub(crate) fn asl_mem(&mut self, address_mode: AddressMode) {
+        self.shift_address(address_mode, |r, v| r.asl(v));
     }
 
-    /// Logical shift right.
-    pub(crate) fn lsr(&mut self, address_mode: AddressMode) {
-        let address = self.address(address_mode);
-        let value = self.read_raw(address);
-
-        let new_carry_flag_value = value & 0b10000000 == 0;
+    fn lsr(&mut self, value: u8) -> u8 {
+        let new_carry_flag_value = (value & 0b00000001) != 0;
         let result = value >> 1;
         self.registers.set_flag_to(Flag::Carry, new_carry_flag_value);
 
         self.check_value_set_zero(result);
         self.check_value_set_negative(result);
 
-        self.write_raw(address, result);
+        result
     }
 
-    /// Rotate left.
-    pub(crate) fn rol(&mut self, address_mode: AddressMode) {
-        let address = self.address(address_mode);
-        let value = self.read_raw(address);
+    /// Logical shift right on accumulator.
+    pub(crate) fn lsr_acc(&mut self) {
+        self.shift_accumulator(|r, v| r.lsr(v));
+    }
 
-        let new_carry_flag_value = value & 0b10000000 == 0;
+    /// Logical shift right.
+    pub(crate) fn lsr_mem(&mut self, address_mode: AddressMode) {
+        self.shift_address(address_mode, |r, v| r.lsr(v));
+    }
+
+    fn rol(&mut self, value: u8) -> u8 {
+        let new_carry_flag_value = value & 0b10000000 != 0;
         let result = value << 1 | self.registers.get_flag(Flag::Carry) as u8;
         self.registers.set_flag_to(Flag::Carry, new_carry_flag_value);
 
         self.check_value_set_zero(result);
         self.check_value_set_negative(result);
 
-        self.write_raw(address, result);
+        result
     }
 
-    /// Rotate right.
-    pub(crate) fn ror(&mut self, address_mode: AddressMode) {
-        let address = self.address(address_mode);
-        let value = self.read_raw(address);
+    /// Rotate left on accumulator.
+    pub(crate) fn rol_acc(&mut self) {
+        self.shift_accumulator(|r, v| r.rol(v));
+    }
 
-        let new_carry_flag_value = value & 0b00000001 == 0;
+    /// Rotate left.
+    pub(crate) fn rol_mem(&mut self, address_mode: AddressMode) {
+        self.shift_address(address_mode, |r, v| r.rol(v));
+    }
+
+    fn ror(&mut self, value: u8) -> u8 {
+        let new_carry_flag_value = value & 0b00000001 != 0;
         let result = value >> 1 | (self.registers.get_flag(Flag::Carry) as u8) << 7;
         self.registers.set_flag_to(Flag::Carry, new_carry_flag_value);
 
         self.check_value_set_zero(result);
         self.check_value_set_negative(result);
 
-        self.write_raw(address, result);
+        result
+    }
+
+    /// Rotate right on accumulator.
+    pub(crate) fn ror_acc(&mut self) {
+        self.shift_accumulator(|r, v| r.ror(v));
+    }
+
+    /// Rotate right.
+    pub(crate) fn ror_mem(&mut self, address_mode: AddressMode) {
+        self.shift_address(address_mode, |r, v| r.ror(v));
     }
 }
