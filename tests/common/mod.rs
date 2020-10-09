090e89c570719008b4c2cd8bfc41d575155973f3
Robert <r.h.w.tenbrincke@gmail.com>
2020-10-09 21:08:53 +0200

    Assembler (#2)
    * Assembler
    
    * Parse numbers, basic addresses
    
    * Remove experimental assembler
    
    * Remove

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -88,22 +88,3 @@ impl TestAssertions for Core<TestCartridge> {
         assert_eq!(self.registers.accumulator as i8, expected)
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use crate::common::{test_with_flags, TestAssertions};
-    use nes::processor::registers::Flag;
-
-    #[test]
-    fn test_setup_flags_no_flags() {
-        let t = test_with_flags(vec![], vec![]);
-        t.assert_flags_set(vec![]);
-    }
-
-    #[test]
-    fn test_setup_flags() {
-        let v = vec![Flag::Carry, Flag::Overflow];
-        let t = test_with_flags(vec![], v.clone());
-        t.assert_flags_set(v);
-    }
-}
